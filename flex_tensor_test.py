452c8a8e0e7df9655214491935cbf60aac44bb1d
Carson Ray <carsonray314@gmail.com>
2024-08-27 00:02:59 -0400

    Added much more argument functionality

--- flex_tensor_test.py
+++ flex_tensor_test.py
@@ -5,23 +5,23 @@
 import numpy as np
 from flextensor import FlexTensor
 
-# Creates axTensors for movie ratings
 print("Movie Ratings:")
 print()
 
+# Creates flextensor with customers on the 0 axis and their associated values on the 1 axis
 customers = FlexTensor(
     [[0, 0.5, 1],
     [1, 0.5, 0.5],
     [0, 1, 0],
     [1, 1, 1]], "customers", "values")
 
+# Creates flextensor with movies on the 0 axis and their associated values on the 1 axis 
 movies = FlexTensor(
     [[0, 1, 0.5],
     [1, 0, 0],
     [0.5, 0.5, 0.5]], "movies", "values")
 
 # Describes axTensors
-
 def describe(tensor):
     print(tensor)
     print("Shape: {}".format(tensor.shape()))
@@ -36,20 +36,18 @@ describe(movies)
 
 print()
 
-# Performes tensor operation
-ratings = customers.by("customers", "values") @ movies.by("values", "movies")
-ratings = FlexTensor(ratings, "customers", "movies")
+# Performes the dot product of the two matrices where you can intuitively see that the end result is movies vs. customers
+ratings = customers["customers", "values"] @ movies["values", "movies"]
 
 describe(ratings)
 
 print()
 
 
-
-# Creates axTensors for multiplication table
 print("Multiplication Table:")
 print()
 
+# Creates vectors that will become the x and y axes of the multiplication table
 x = FlexTensor(np.arange(10), "x")
 y = FlexTensor(np.arange(10), "y")
 
@@ -61,19 +59,49 @@ describe(y)
 
 print()
 
-# Multiplies in cartesian product form
-table = FlexTensor(y.by("y", [1]) * x.by([1], "x"), "y", "x")
+# Performes a cartesian product by adding placeholder axes at the right locations
+table = y["y", ""] * x["", "x"]
 
 describe(table)
 
 print()
 
-# Creates cubic table
-table = FlexTensor(table.by([1], "y", "x") * x.by("x", [2]), "z", "y", "x")
+# Creates cubic table through the same process
+table = FlexTensor(table["", "y", "x"] * x["x", "", ""], "z", "y", "x")
 
 describe(table)
 
 print()
 
+# Demonstrates use of ellipsis
+foo = FlexTensor(np.arange(16).reshape(2, 2, 2, 2), "a", "b", "c", "d")
 
-    
+describe(foo)
+
+# Note: result is returned as flextensor instead of numpy array
+foo = foo["d", ..., "", "a"]
+
+describe(foo)
+
+print()
+
+# Demonstrates fancy indexing
+foo = FlexTensor(np.arange(27).reshape(3, 3, 3), "z", "y", "x")
+
+describe(foo)
+print()
+
+foo = foo[..., ("y", 0), ("x", slice(2))]
+
+describe(foo)
+
+# Demonstrates flattening multiple axes into each other from right to left in the list argument
+foo = FlexTensor(np.arange(16).reshape(2,2,2,2), "a", "b", "c", "d")
+
+describe(foo)
+print()
+
+foo = foo[("c", 0), ["b", "d", "a"]]
+
+describe(foo)
+
--- flextensor.py
+++ flextensor.py
@@ -3,29 +3,44 @@ import numpy as np
 class FlexTensor:
     def __init__(self, array, *axes):
         """A multi-dimensional array construct with labeled axes"""
-        # Defines tensor values from numpy array
-        self.vals = np.array(array)
+        # Defines tensor values from flextensor or numpy array
+        try:
+            self.vals = array.vals.copy()
+        except AttributeError:
+            self.vals = np.array(array)
         
         # Defines axes of tensor
         self.axes = list(axes)
 
         if len(self.axes) > len(self.shape()):
             # Fills out shape if there are more axes than current shape
-            self.vals = self.by([len(self.axes) - len(self.shape())])
+            self.vals = self[*[""]*(len(self.axes) - len(self.shape()))]
         else:
             # Appends axis numbers to axes if not all axes are defined
-            self.axes = self.axes + list(range(len(self.axes), len(self.shape())))
+            self.axes = self.axes + [""]*(len(self.shape())-len(self.axes))
 
+    def set_ax(self, axes):
+        self.axes = axes
+    
+    def merge_ax(self, axes):
+        # Fills in placeholders with another axis list
+        return [other if own == "" else own for own, other in zip(self.axes, axes)]
+
+    def pivot_ax(self, axes):
+        return [self.axes[0], axes[1]]
         
     def ax(self, *labels):
         """Determines axis number of label(s)"""
         axes = []
         
         for label in labels:
-            try:
-                # Tries finding string label
-                axes.append(self.axes.index(label))
-            except ValueError:
+            if isinstance(label, str):
+                try:
+                    # Tries finding string label
+                    axes.append(self.axes.index(label))
+                except ValueError:
+                    axes.append(None)
+            else:
                 # Uses actual index of axis
                 axes.append(int(label))
 
@@ -35,8 +50,8 @@ class FlexTensor:
         else:
             return axes
     
-    def by(self, *args):
-        """Selects axes of a tensor to return a numpy array"""
+    def __getitem__(self, args):
+        """Selects axes of tensor"""
         
         # Sets result array to a copy of tensor values
         result = self.vals.copy()
@@ -44,60 +59,128 @@ class FlexTensor:
         # Sets num to count axis number
         num = 0
 
+        # Ellipsis position
+        ellipsisPos = -1
+
         # New axes start at the end of the result shape
         newStart = len(result.shape)
 
-        # List to hold source of new axes
-        axSource = np.array([], dtype=int)
+        # Map from orginial axis indices to new indices
+        axMap = list(range(len(result.shape)))
 
-        # List to hold destination of new axes
-        axDest = np.array([], dtype=int)
+        # Array to hold source of new axes
+        axSource = []
+
+        # Keeps track of axes that have been referenced
+        referenced = axMap.copy()
         
         # Loops through arguments
         for arg in args:
-            if isinstance(arg, list):
-                # If argument is newaxis arg specified by
-                # [num] where num is number of new axes
-                
-                # Gets number of new axes
-                new = arg[0]
-                
-                # Creates full slices along length of axes
-                slices = [np.index_exp[:][0]]*(len(result.shape))
-                
-                # Adds axes to array plus the amount of new axes
-                result = result[tuple(slices + [np.newaxis]*new)]
-
-                # Adds the new axes to the axis order
-                axSource = np.append(axSource, np.arange(newStart, newStart + new))
-
-                # Adds current axis numbers to the axis destination
-                axDest = np.append(axDest, np.arange(num, num + new))
-                
-                # Increments num and newCount by number of new axes
-                num += new
-
-                newStart += new
-                
-            elif arg is Ellipsis:
-                # If argument is ellipsis, assume that all axis referencing is complete
-                # Sets axis num to the current shape
-                num = len(result.shape)
+            if arg is Ellipsis:
+                # If argument is ellipsis, sets position
+                ellipsisPos = num
                 
             else:
-                # Argument is axis label
-                
-                # Swaps axis specified by label with current position
-                axSource = np.append(axSource, [self.ax(arg)])
+                # If indexing parameter is included
+                if isinstance(arg, tuple):
+                    # List of slices to be performed (full slices at first)
+                    slices = [np.index_exp[:][0]]*(len(result.shape))
+                    # Inserts desired slice at axis
+                    slices[axMap.index(self.ax(arg[0]))] = arg[1]
 
-                # Adds current axis num
-                axDest = np.append(axDest, [num])
+                    # Performes slices
+                    currLen = len(result.shape)
+                    result = result[tuple(slices)]
+
+                    arg = arg[0]
+                    # Checks to see if axis was removed
+                    if len(result.shape) < currLen:
+                        # Clears from referenced list
+                        referenced.remove(self.ax(arg))
+                        axMap.remove(self.ax(arg))
+                        # Decrements new axis start position
+                        newStart -= 1
+                        # Moves to next axis
+                        continue
+                    
+                
+                elif isinstance(arg, list):
+                    # Flattens desired axes together into first axis
+
+                    # Flattens arrays into each other backwards
+                    for s, t in zip(arg[1:][::-1], arg[:-1][::-1]):
+                        source = axMap.index(self.ax(s))
+                        target = axMap.index(self.ax(t))
+                        # Splits array into blocks along source axis
+                        blocks = np.split(result, result.shape[source], axis=source)
+                        # Concatenates blocks onto target axis
+                        result = np.concatenate(blocks, axis=target)
+                        # Reshapes to remove source axis
+                        shape = list(result.shape)
+                        shape.pop(source)
+                        result = result.reshape(shape)
+                        # Clears from referenced list
+                        referenced.remove(self.ax(s))
+                        axMap.remove(self.ax(s))
+                        # Decrements new axis start position
+                        newStart -= 1
+
+                    arg = arg[0]
+
+                elif arg == "":
+                    # If argument is new axis
+                    
+                    # Creates full slices along length of axes
+                    slices = [np.index_exp[:][0]]*(len(result.shape))
+                    
+                    # Adds axes to array plus the new axis
+                    result = result[tuple(slices + [np.newaxis])]
+
+                    # Adds the new axis to the axis order
+                    axSource.append(newStart)
+                    axMap.append(newStart)
+                    
+                    # Increments num and newCount
+                    num += 1
+                    newStart += 1
+
+                    # Moves to next axis
+                    continue
+
+                # Argument is axis label
+                # Swaps axis specified by label with current position
+                axSource.append(self.ax(arg))
                 
                 # Increments num
                 num += 1
+
+                # Removes from reference list
+                referenced.remove(self.ax(arg))
+
+        # Creates axis index
+        axIndex = np.array([axMap.index(ax) for ax in axSource])
+
+        # Creates reference index
+        refIndex = np.array([axMap.index(ax) for ax in referenced])
+        
+        # Splices in unreferenced axes at ellipsis parameter
+        if ellipsisPos > -1 and len(refIndex) > 0:
+            axIndex = np.concatenate((axIndex[:ellipsisPos], refIndex, axIndex[ellipsisPos:]))
             
+            # Moves num to end of axis list
+            num = newStart
+
         # Transposes result with new axOrder mapped to the sequence of new axes
-        return np.moveaxis(result, source=axSource, destination=axDest)
+        result = np.moveaxis(result, source=axIndex, destination=np.arange(num))
+        
+        # Adds new axes
+        axes = self.axes + [""]*(len(result.shape)-len(self.axes))
+        
+        # Transposes axes
+        mixAxes = [axes[axMap[ax]] for ax in axIndex]
+
+        # Returns new flextensor
+        return FlexTensor(result, *mixAxes)
 
     def shape(self, *axes):
         """Returns the shape of the tensor along axes"""
@@ -111,9 +194,24 @@ class FlexTensor:
     def size(self, *axes):
         """Returns the size of the tensor along axes"""
         return np.prod(self.shape(*axes))
-
-    # Magic methods
+    
     def __str__(self):
         """Returns string representation"""
         string = f"AxTensor(\n{str(self.vals)}, {self.axes})"
         return string
+    
+
+    def __add__(self, other):
+        return FlexTensor(self.vals + other.vals, *self.merge_ax(other.axes))
+    
+    def __sub__(self, other):
+        return FlexTensor(self.vals - other.vals, *self.merge_ax(other.axes))
+    
+    def __mul__(self, other):
+        return FlexTensor(self.vals * other.vals, *self.merge_ax(other.axes))
+    
+    def __truediv__(self, other):
+        return FlexTensor(self.vals / other.vals, *self.merge_ax(other.axes))
+    
+    def __matmul__(self, other):
+        return FlexTensor(self.vals @ other.vals, *self.pivot_ax(other.axes))
